ad787a6a12f74eae4cdbed9ffb91b92abfa6ff07
Sudhakar <sssudhakar1985@gmail.com>
2019-07-03 10:22:41 +0530

    signalr changes

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+import { SignalRService } from '../services/signal-r.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,11 +10,47 @@ import { Title } from '@angular/platform-browser';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private title: Title) {
+  public chartOptions: any = {
+    scaleShowVerticalLines: true,
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+  public chartLabels: string[] = ['Real time data for the chart'];
+  public chartType = 'bar';
+  public chartLegend = true;
+  public colors: any[] = [
+    { backgroundColor: '#5491DA' },
+    { backgroundColor: '#E74C3C' },
+    { backgroundColor: '#82E0AA' },
+    { backgroundColor: '#E5E7E9' }];
+
+  constructor(public signalRService: SignalRService, private http: HttpClient, private title: Title) {
     this.title.setTitle('Keep Note - Dashboard');
-   }
+  }
 
   ngOnInit() {
+    this.signalRService.startConnection();
+    this.signalRService.addTransferChartDataListener();
+    this.signalRService.addBroadcastChartDataListener();
+    this.startHttpRequest();
+  }
+
+  private startHttpRequest = () => {
+    this.http.get('http://localhost:60967/api/chart')
+      .subscribe(res => {
+        console.log(res);
+      })
+  }
+
+  public chartClicked = (event: any) => {
+    console.log(event);
+    this.signalRService.broadcastChartData();
   }
 
 }
