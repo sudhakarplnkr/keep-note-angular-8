65c50070969974fb1c18e733f58d747202532f16
Sudhakar Sivananaindha Perumal <466590@cognizant.com>
2019-07-03 06:53:42 +0000

    changes for chart data - API

---
+++ backend/NoteService/Controllers/ChartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using NoteService.DataStorage;
+using NoteService.HubConfig;
+using NoteService.TimerFeatures;
+
+namespace NoteService.Controllers
+{
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private IHubContext<ChartHub> _hub;
+
+        public ChartController(IHubContext<ChartHub> hub)
+        {
+            _hub = hub;
+        }
+
+        [Route("api/chart")]
+        public IActionResult Get()
+        {
+            var timerManager = new TimerManager(() => _hub.Clients.All.SendAsync("transferchartdata", DataManager.GetData()));
+
+            return Ok(new { Message = "Request Completed" });
+        }
+    }
+}
---
+++ backend/NoteService/HubConfig/ChartHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using NoteService.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NoteService.HubConfig
+{
+    public class ChartHub: Hub
+    {
+        public async Task BroadcastChartData(List<ChartModel> data) => await Clients.All.SendAsync("broadcastchartdata", data);
+    }
+}
